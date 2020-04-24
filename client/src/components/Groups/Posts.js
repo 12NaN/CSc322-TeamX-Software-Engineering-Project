9fe756512a654bfe804b6f9cc60033172b327537
[12NaN] <frank.orefice25@students.kbcc.cuny.edu>
2020-04-24 18:01:27 -0400

    Posts chat

--- client/src/components/Groups/Posts.js
+++ client/src/components/Groups/Posts.js
@@ -1,13 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import io from "socket.io-client";
 
-class Posts extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Posts</h1>
-            </div>
-        );
-    }
-}
+let endPoint = "http://localhost:5000";
+let socket = io.connect(`${endPoint}`);
+
+function Posts() {
+    const [messages, setMessages] = useState(["Hello And Welcome"]);
+    const [message, setMessage] = useState("");
+  
+    useEffect(() =>{
+     getMessages();
+    }, [messages.length]);
+  
+    const getMessages = () =>{
+     socket.on("message", msg =>{
+      setMessages([...messages, msg]);
+     });
+    };
+  
+    const onChange = e => {
+     setMessage(e.target.value);
+    };
+    
+    const onClick = () => {
+     if(message !== ""){
+      socket.emit("message", message);
+      setMessage("");
+     }
+     else
+      alert("Please Add a Message");
+    };
+    return (
+      <div>
+        {messages.length > 0 && messages.map(msg =>(
+         <div>
+              <p>{msg}</p>
+             </div>
+        ))}
+        <input value= {message} name="message" onChange={e => onChange(e)}/>
+        <button onClick={() => onClick()}>Send Message</button>
+  
+      </div>
+    );
+  }
 
 export default Posts;
