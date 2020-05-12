1610c617cde7ab2a5c750fe2eadbc6c147abad49
Peter Ye <47184879+PeterYe100@users.noreply.github.com>
2020-05-12 05:28:25 -0400

    completed poll meeting

--- client/src/components/Groups/PollSection.js
+++ client/src/components/Groups/PollSection.js
@@ -1,7 +1,10 @@
 import React, { Component, useState, useEffect } from 'react';
 import io from "socket.io-client";
 import {Link} from 'react-router-dom';
-
+import {Button, ListGroup} from 'react-bootstrap';
+import PollCards from './pollCards'
+import axios from 'axios';
+import './PollSection.css';
 
 //let endPoint = "http://localhost:5000";
 //let socket = io.connect(`${endPoint}`);
@@ -9,19 +12,42 @@ class PollSection extends Component {
     constructor(props){
         super(props);
         this.state = {
-            group_id: this.props.group
+            group_id: this.props.group,
+            polls: [],
+            data: true
+
         };
     }
-    
+    componentDidMount() {
+       
+        axios.get(`/projects/${this.state.group_id}`)
+       .then((response) =>{
+           console.log(response.data['Polls'])
+           this.setState({
+            polls: [...response.data['Polls']].reverse(),
+            data: true
+
+           })
+           console.log(this.state.polls)
+       })
+    }
     
     render() {
-
+        const listItems = this.state.polls.map((i) =>
+            <PollCards pollname={i["desc"]} poll_id={i["poll_id"]} group_id={this.state.group_id}/>
+        );
         return (
             <div>
-                <h1>Available Polls</h1>
+                <h1>Available Meeting Polls</h1>
+                <br/>
+                <div className='pollsection'>
+                <ListGroup>{listItems}</ListGroup>
+                </div>
+                <br/>
+                <br/>
                  <Link style={{"backgroundColor": "purple"}} className='btn btn-dark btn-block'to={{pathname: '/projects/' + this.props.group + '/createpoll'}}>
-                    Create Poll
-                 </Link>    
+                            Create Poll
+                    </Link>                
             </div>
         );
     }
