1c8fe377175e2af10fedb89086ab74c1e7945c73
Peter Ye <47184879+PeterYe100@users.noreply.github.com>
2020-05-13 09:08:01 -0400

    added vote cards

--- client/src/components/Groups/VoteSection.js
+++ client/src/components/Groups/VoteSection.js
@@ -2,9 +2,10 @@ import React, { Component, useState, useEffect } from 'react';
 import io from "socket.io-client";
 import {Link} from 'react-router-dom';
 import {Button, ListGroup} from 'react-bootstrap';
-import PollCards from './pollCards'
+import VoteCards from './voteCards'
 import axios from 'axios';
 import './PollSection.css';
+
 
 //let endPoint = "http://localhost:5000";
 //let socket = io.connect(`${endPoint}`);
@@ -13,7 +14,7 @@ class VoteSection extends Component {
         super(props);
         this.state = {
             group_id: this.props.group,
-            polls: [],
+            votes: [],
             data: true
 
         };
@@ -22,28 +23,32 @@ class VoteSection extends Component {
        
         axios.get(`/projects/${this.state.group_id}`)
        .then((response) =>{
-           console.log(response.data['Polls'])
+           console.log(response.data['Vote'])
            this.setState({
-            polls: [...response.data['Polls']].reverse(),
+            votes: [...response.data['Vote']].reverse(),
             data: true
 
            })
            console.log('hello')
-           console.log(this.state.polls)
+           console.log(this.state.votes)
            console.log('hi')
        })
+
+
     }
     
     render() {
-        const listItems = this.state.polls.map((i) =>
-            <PollCards pollname={i["desc"]} poll_id={i["poll_id"]} group_id={this.state.group_id}/>
+        const listItems = this.state.votes.map((i) =>
+            <VoteCards votename={i["desc"]} vote_id={i["vote_id"]} group_id={this.state.group_id} yes_votes={i["vote_yes"]} no_votes={i["vote_no"]} vote_type={i["vote_type"]} subject_user={i["user_id_subject"]}/>
         );
         return (
             <div>
-                <h1>Available Meeting Polls</h1>
+                <h1>Available Votes</h1>
+                <br></br>
+                <h4>Make sure you vote! </h4>
                 <br/>
                 <div className='pollsection'>
-                <ListGroup></ListGroup>
+                <ListGroup>{listItems}</ListGroup>
                 </div>
                 <br/>
                 <br/>
